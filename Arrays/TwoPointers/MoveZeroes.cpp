d7d18401297e04026623d6342b6b32bbb927aea3
Kunal Jain <96196312+Kuna1jain@users.noreply.github.com>
2026-07-05 10:55:43 +0530

    Add MoveZeroes solution to move zeroes in array
    Implement algorithm to move all zeroes in an array to the end while maintaining the order of non-zero elements.

---
+++ Arrays/TwoPointers/MoveZeroes.cpp
@@ -0,0 +1,18 @@
+// Problem: Move Zeroes
+// Link: https://leetcode.com/problems/move-zeroes/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        int left = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+
+            if (nums[right] != 0) {
+                swap(nums[left], nums[right]);
+                left++;
+            }
+        }
+    }
+};
