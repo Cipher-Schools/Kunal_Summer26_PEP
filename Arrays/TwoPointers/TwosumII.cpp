df827f6f7d83be4869fcf1e97f776956f46b4d79
Kunal Jain <96196312+Kuna1jain@users.noreply.github.com>
2026-07-05 10:56:37 +0530

    Add Two Sum II solution in TwosumII.cpp
    Implement Two Sum II solution using two pointers.

---
+++ Arrays/TwoPointers/TwosumII.cpp
@@ -0,0 +1,26 @@
+// Problem: Two Sum II - Input Array Is Sorted
+// Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+
+        int left = 0;
+        int right = numbers.size() - 1;
+
+        while (left < right) {
+
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target)
+                return {left + 1, right + 1};
+
+            if (sum < target)
+                left++;
+            else
+                right--;
+        }
+
+        return {};
+    }
+};
